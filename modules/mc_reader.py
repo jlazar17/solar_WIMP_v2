5eab8a5a7fd4f3f864317d5062fa52c2917c7a33
jlazar17 <jeff.p.lazar@gmail.com>
2021-03-01 12:22:49 -0600

    update mc_reader to latest

--- modules/mc_reader.py
+++ modules/mc_reader.py
@@ -1,84 +1,82 @@
 import numpy as np
-import h5py
-from os import path
+from helper_functions import mc_fname
+from make_mask import make_mask
 
-from controls import oscNext_nfiles 
+def mc_name(path):
+    if 'hmniederhausen/' in path:
+        name = 'intracks'
+    elif ('oscNext' in path):
+        name = 'oscNext'
+    elif 'JLevel' in path:
+        if 'nancy' in path:
+            name = 'nancy'
+        elif 'genie' in path:
+            name = 'genie'
+        elif 'corsika' in path:
+            name = 'corsika'
+    else:
+        raise RuntimeError('Event selection not recognized')
+    return name
 
+def load_core_data(selection, path):
+    from core_data_loaders import core_data_loaders
+    if selection in core_data_loaders.keys():
+        data = core_data_loaders[selection](path)
+    else:
+        raise RuntimeError('Do not know how to load %s' % path)
+    return data
 
-class MCReader():
+def check_same_len(mc_data):
+    lst = [len(t[0]) for t in mc_data]
+    return len(set(lst))==1
 
-    def __init__(self, path, slc=slice(None), options='00'):
-        self.path      = path
-        self.fname     = '.'.join(path.split('/')[-1].split('.')[:-1])
-        self._rescale  = int(options[0])
-        self._scramble = int(options[1])
-        self.slc       = slc
-        self.set_event_selection()
-        
-        if self.event_selection=='intracks':
-            self.mcf = np.load(self.path)
-        elif self.event_selection=='oscNext':
-            self.mcf = h5py.File(self.path, 'r')
+class MCReader(object):
 
-        self.set_mc_quantities()
+    def __init__(self, path=None, name=None, options='00', additional_data=None):
 
-        if self.event_selection=='oscNext':
-            self.mcf.close()
-
-    def set_event_selection(self):
-        if 'hmniederhausen/' in self.path:
-            self.event_selection = 'intracks'
-        elif ('oscNext' in self.path):
-            self.event_selection = 'oscNext'
+        self.options = options
+        self.path    = path
+        if path is None and additional_data is None:
+            raise RuntimeError('Must specify either a path or data')
+        elif path is not None:
+            self.fname = mc_fname(path)
+            self.name = mc_name(path)
+            mc_data = load_core_data(self.name, path)
         else:
-            print('Event selection not recognized')
-            quit()
+            self.name = name
+            mc_data = []
+        if additional_data is not None:
+            if type(additional_data)==tuple:
+                mc_data.append(additional_data)
+            else:
+                for data_desc in additional_data:
+                    mc_data.append(data_desc)
+        if not check_same_len(mc_data):
+            raise RuntimeError('All data must be the same length')
+        arr = np.array([tup for tup in zip(*tuple([t[0] for t in mc_data]))],
+                       dtype=[(t[1], t[2]) for t in mc_data]
+                      )
+        self._mc_data = arr.view(np.recarray)
+        self.mc_data  = self._mc_data
 
-    def set_mc_quantities(self):
-        if self.event_selection=='intracks':
-            self.nu_e      = self.mcf['trueE'][self.slc]
-            self.nu_az     = self.mcf['trueAzi'][self.slc]
-            self.nu_zen    = self.mcf['trueDec'][self.slc]+np.pi/2
-            self.reco_e    = np.power(10, self.mcf['logE'])[self.slc]
-            self.reco_az   = self.mcf['azi'][self.slc]
-            self.reco_zen  = self.mcf['dec'][self.slc]+np.pi/2
-            self.oneweight = self.mcf['ow'][self.slc]
-            self.ptype     = np.where(np.random.rand(len(self.nu_az))<0.4511734444723442, 14,-14)
-        elif self.event_selection=='oscNext':
-            self.nu_e      = []
-            self.nu_az     = []
-            self.nu_zen    = []
-            self.reco_e    = []
-            self.reco_az   = []
-            self.reco_zen  = []
-            self.ptype     = []
-            self.trackprob = []
-            self.oneweight = []
-            self.rweight   = []
-            for key in self.mcf.keys():
-                self.nu_e      = np.append(self.nu_e, self.mcf[key]['MCInIcePrimary.energy'][()])
-                self.nu_az     = np.append(self.nu_az, self.mcf[key]['MCInIcePrimary.dir.azimuth'][()])
-                self.nu_zen    = np.append(self.nu_zen, np.arccos(self.mcf[key]['MCInIcePrimary.dir.coszen'][()]))
-                self.reco_e    = np.append(self.reco_e, self.mcf[key]['L7_reconstructed_total_energy'][()])
-                self.reco_az   = np.append(self.reco_az, self.mcf[key]['L7_reconstructed_azimuth'][()])
-                self.reco_zen  = np.append(self.reco_zen, np.arccos(self.mcf[key]['L7_reconstructed_coszen'][()]))
-                self.ptype     = np.append(self.ptype, self.mcf[key]['MCInIcePrimary.pdg_encoding'][()])
-                self.trackprob = np.append(self.trackprob, self.mcf[key]['L7_PIDClassifier_FullSky_ProbTrack'][()])
-                self.oneweight = np.append(self.oneweight, self.mcf[key]['I3MCWeightDict.OneWeight'][()] / (self.mcf[key]['I3MCWeightDict.NEvents'][()] * self.mcf[key]['I3MCWeightDict.gen_ratio'][()] * oscNext_nfiles[key]))
-                self.rweight   = np.append(self.rweight, self.mcf[key]['ReferenceWeight'][()])
-            self.nu_e      = self.nu_e[self.slc]
-            self.nu_az     = self.nu_az[self.slc] 
-            self.nu_zen    = self.nu_zen[self.slc] 
-            self.reco_e    = self.reco_e[self.slc] 
-            self.reco_az   = self.reco_az[self.slc] 
-            self.reco_zen  = self.reco_zen[self.slc] 
-            self.ptype     = self.ptype[self.slc] 
-            self.trackprob = self.trackprob[self.slc]
-            self.oneweight = self.oneweight[self.slc]
-            self.rweight   = self.rweight[self.slc]
+    def __len__(self):
+        return len(self.mc_data)
 
-if __name__=='__main__':
-    from  sys import argv as args
-    path = args[1]
-    mcr = MCReader(path)
-    print(len(mcr.nu_e))
+    def __getitem__(self, key):
+        return self.mc_data[key]
+
+    def __add__(self, other):
+        shared_dnames = list(set(self._mc_data.dtype.names) & set(other._mc_data.dtype.names))
+        data          = [(np.append(self._mc_data[name], other._mc_data[name]), name, dtype)
+                         for name, dtype in self._mc_data.dtype.descr
+                         if name in shared_dnames
+                        ]
+        name = '%s-%s' % (self.name, other.name)
+        return MCReader(additional_data=data, name=name)
+    def unmask(self):
+        self.mc_data = self._mc_data   
+    def make_mask(self, cuts):
+        return make_mask(self._mc_data, cuts)
+
+    def apply_mask(self, mask):
+        self.mc_data = self._mc_data[mask]
